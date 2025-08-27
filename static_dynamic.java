b1ba624be1ef4c042081c34918762961fd6a237d
Abhay Verma <vabhay22435@gmail.com>
2025-08-27 12:39:22 +0530

    comit

---
+++ static_dynamic.java
@@ -0,0 +1,10 @@
+// import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class static_dynamic {
+    public static void main(String[] args){
+        int[] d_array = new int[5];
+        int[] a = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(a));
+    }
+}
